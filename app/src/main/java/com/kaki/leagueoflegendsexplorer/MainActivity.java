3a895f8e821a9c1f294a16dafa8be8ad6755d48a
Kakikou <kevin1.rodrigues@epitech.eu>
2015-07-04 20:39:59 +0200

    Add toolbar and link ListChamp to DetailChamp

--- app/src/main/java/com/kaki/leagueoflegendsexplorer/MainActivity.java
+++ app/src/main/java/com/kaki/leagueoflegendsexplorer/MainActivity.java
@@ -1,19 +1,40 @@
 package com.kaki.leagueoflegendsexplorer;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import com.kaki.leagueoflegendsexplorer.fragments.champions.ListChampionsFragment;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import com.kaki.leagueoflegendsexplorer.api.riot.champion_v1_2.models.ChampionDto;
+import com.kaki.leagueoflegendsexplorer.fragments.champions.ListChampionsFragment;
+import com.kaki.leagueoflegendsexplorer.interaction.LaunchFragment;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements LaunchFragment {
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
 
         getSupportFragmentManager().beginTransaction()
-                .addToBackStack(null)
                 .add(R.id.fragment_container, ListChampionsFragment.newInstance())
+                .commit();
+    }
+
+    @Override
+    public void launchFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .addToBackStack(null)
+                .replace(R.id.fragment_container, fragment)
                 .commit();
     }
 }
