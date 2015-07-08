d3d6dce662841a33c6f08a524f1308b947cc7ce8
Kakikou <kevin1.rodrigues@epitech.eu>
2015-07-09 01:04:15 +0200

    Update server

--- app/src/main/java/com/kaki/leagueoflegendsexplorer/api/riot/UrlImage.java
+++ app/src/main/java/com/kaki/leagueoflegendsexplorer/api/riot/UrlImage.java
@@ -5,9 +5,9 @@ package com.kaki.leagueoflegendsexplorer.api.riot;
  */
 public class UrlImage {
 
-    public static final String CHAMPION_URL = "http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/";
+    public static final String CHAMPION_URL = "http://ddragon.leagueoflegends.com/cdn/5.12.1/img/champion/";
     public static final String CHAMPION_SCREEN_ART_URL = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/";
     public static final String CHAMPION_SPLASH_SKIN = "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/";
-    public static final String SKILL_URL = "http://ddragon.leagueoflegends.com/cdn/5.2.1/img/spell/";
-    public static final String PASSIVE_SKILL_URL = "http://ddragon.leagueoflegends.com/cdn/5.2.1/img/passive/";
+    public static final String SKILL_URL = "http://ddragon.leagueoflegends.com/cdn/5.12.1/img/spell/";
+    public static final String PASSIVE_SKILL_URL = "http://ddragon.leagueoflegends.com/cdn/5.12.1/img/passive/";
 }
