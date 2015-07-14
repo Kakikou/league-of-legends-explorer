978ee363020afb61dd2ec36e96f6d1ab82197b61
Kakikou <kevin1.rodrigues@epitech.eu>
2015-07-14 19:33:09 +0200

    Add keystore

--- app/src/main/java/com/kaki/leagueoflegendsexplorer/MyApplication.java
+++ app/src/main/java/com/kaki/leagueoflegendsexplorer/MyApplication.java
@@ -2,9 +2,11 @@ package com.kaki.leagueoflegendsexplorer;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Logger;
 import com.google.android.gms.analytics.Tracker;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by kevinrodrigues on 09/07/15.
@@ -17,9 +19,9 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics.Builder().disabled(BuildConfig.DEBUG).build());
         analytics = GoogleAnalytics.getInstance(this);
         analytics.setLocalDispatchPeriod(1800);
-
         analytics.getLogger().setLogLevel(Logger.LogLevel.VERBOSE);
         tracker = analytics.newTracker(R.string.google_analytics);
         tracker.enableExceptionReporting(true);
